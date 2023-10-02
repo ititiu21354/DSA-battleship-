7645d4410c7ce7ae119afd25e42f4b451e2e71df
ititiu21354@student.hcmiu.edu.vn <ititiu21354@student.hcmiu.edu.vn>
2023-10-02 11:40:28 +0700

    Push code to git

---
+++ src/Ship.java
@@ -0,0 +1,26 @@
+class Ship {
+    private int[] locationCells;
+    private int numOfHits;
+
+    public String checkYourself(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "miss";
+        for(int cell : locationCells) {
+            if(guess == cell){
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length){
+            result = "kill";
+        }
+        System.out.println(result);
+        return result; 
+
+    }
+
+    public void setLocationCells(int[] locations){
+        this.locationCells = locations;
+    }
+}
